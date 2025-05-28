874b579d799c2cd2f859ba808b3e919cd322c2bd
Bill Jones <williamjones120862@gmail.com>
2025-05-28 10:40:53 -0600

    Added sign in, log in and log out functionality

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 * Ruby version -  3.0.3
 
 * Rails version -  7.1.5.1
+
+Followed tutorial at https://sdrmike.medium.com/rails-7-api-only-app-with-devise-and-jwt-for-authentication-1397211fb97c
+to set up JWT. In the section Set up devise-jwt the command 'EDITOR='code --wait' rails credentials:edit' did not work. It
+just saved the credentials file immediately without opening it in the editor. I needed to include 'code' (i.e VSCode) in 
+the command path. To do this, in VSCode go to View, Command Pallet, then enter 'Shell Command: Install 'code' in path' 
+and click. The 'EDITOR='code --wait' rails credentials:edit' command should then work and an editable version of the credentials.yml file should open in VSCode
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_27_154439) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_28_151657) do
   create_table "payments", force: :cascade do |t|
     t.string "description"
     t.float "amount"
@@ -27,7 +27,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_27_154439) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
